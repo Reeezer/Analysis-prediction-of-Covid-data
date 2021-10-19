804a4933f5f7ab2878430adcbb4be5f10d448974
Léon Muller <leon.muller@he-arc.ch>
2021-10-19 18:23:56 +0200

    Testing different machine learning models

---
+++ toolbox.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+def scatter_plot(X_train, X_test, y_train, y_test, title="sdf"):
+    plt.figure(figsize=(20, 7))
+    plt.scatter(X_train, y_train, color='blue', label='train')
+    plt.scatter(X_test, y_test, color='red', label='test')
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('New deaths')
+    plt.legend()
+    plt.show()
