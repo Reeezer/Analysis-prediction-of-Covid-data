978302b47529ce6438f2a2e49a9450847eec6b09
Léon Muller <leon.muller@he-arc.ch>
2021-10-26 14:25:39 +0200

    Adding random forests & gradient boosting trees, and now using mutliple features to predict new_deaths

--- toolbox.py
+++ toolbox.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def scatter_plot(X_train, X_test, y_train, y_test, title="sdf"):
+def scatter_plot(X_train, X_test, y_train, y_test, title="Title"):
     plt.figure(figsize=(20, 7))
     plt.scatter(X_train, y_train, color='blue', label='train')
     plt.scatter(X_test, y_test, color='red', label='test')
