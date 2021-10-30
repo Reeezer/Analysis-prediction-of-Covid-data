3a0b9717517e5fb51cc70c9ea60cce92ebc61187
Léon Muller <leon.muller@he-arc.ch>
2021-10-31 00:15:43 +0200

    Added prediction value for the next day

--- toolbox.py
+++ toolbox.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 
 
-def scatter_plot(X_train, X_test, y_train, y_test, title="Title"):
+def scatter_plot(X_train, X_test, X_pred, y_train, y_test, y_pred, title="Title"):
     plt.figure(figsize=(20, 7))
     plt.scatter(X_train, y_train, color='blue', label='train')
     plt.scatter(X_test, y_test, color='red', label='test')
+    plt.scatter(X_pred, y_pred, color='green', label='prediction')
     plt.title(title)
     plt.xlabel('Date')
     plt.ylabel('New deaths')
