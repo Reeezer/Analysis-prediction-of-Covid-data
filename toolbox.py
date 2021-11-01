d79c5afccc34de0627226a60feb22689fe578d80
Léon Muller <leon.muller@he-arc.ch>
2021-11-01 16:43:46 +0100

    Changed the prediction display, and now predict new month every day new deaths

--- toolbox.py
+++ toolbox.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def scatter_plot(X_train, X_test, X_pred, y_train, y_test, y_pred, title="Title"):
+    X1, y1 = reshape_order_data(X_train, y_train)
+    X2, y2 = reshape_order_data(X_test, y_test)
+
     plt.figure(figsize=(20, 7))
-    plt.scatter(X_train, y_train, color='blue', label='train')
-    plt.scatter(X_test, y_test, color='red', label='test')
-    plt.scatter(X_pred, y_pred, color='green', label='prediction')
+    plt.fill_between(X1, y1, 0, color='blue', facecolor='cornflowerblue', alpha=0.6, label='train')
+    plt.plot(X2, y2, color='red', linewidth=3, label='test')
+    plt.plot(X_pred, y_pred, color='green', linewidth=3, label='prediction')
     plt.title(title)
     plt.xlabel('Date')
     plt.ylabel('New deaths')
     plt.legend()
     plt.show()
+
+def reshape_order_data(X, y):
+    X_reshaped = X.reshape(-1)
+    y_reshaped = y.reshape(-1)
+    order = np.argsort(X_reshaped)
+    x_ordered = np.array(X_reshaped)[order]
+    y_ordered = np.array(y_reshaped)[order]
+    return x_ordered, y_ordered
